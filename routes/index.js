bd83f97ecd0e9d082c2f20ad2a3ab03087221896
Brad Oyler <bradoyler@gmail.com>
2014-11-10 21:02:05 -0500

    1st go at express content demo

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var jsondata = require('../json/cover.json');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  var data = jsondata;
+  data.title='Cover stories';
+  res.render('index', data);
+});
+
+module.exports = router;
